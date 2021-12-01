cb7dc603be2cf656aebcd5dc884c95696620331a
rodrigodadam <rodrigodadam@gmail.com>
2021-12-01 15:36:53 +0000

    Refact to buildin django form

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
-
-# Create your views here.
+from .forms import ItemForm
 
 
 def get_todo_list(request):
@@ -13,4 +12,13 @@ def get_todo_list(request):
 
 
 def add_item(request):
-    return render(request, 'todo/add_item.html')
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
