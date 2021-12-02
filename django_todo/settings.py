5d24b737a1367fa9f17b77798ccd54c874a87a7c
rodrigodadam <rodrigodadam@gmail.com>
2021-12-02 20:56:16 +0000

    Refatc manage

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -85,14 +85,13 @@ if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
